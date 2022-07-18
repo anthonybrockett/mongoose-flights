91fabd608ad7d8e408c124b7a464b7c33c455f62
anthonybrockett <ajbrockett1991@gmail.com>
2022-07-18 15:27:12 -0700

    Add Destination Functionality

--- controllers/destinations.js
+++ controllers/destinations.js
@@ -1,5 +1,14 @@
-const FLight = require('../models/flight');
+const Flight = require('../models/flight');
 
 module.exports = {
-
+    create
 }
+
+function create(req, res) {
+    Flight.findById(req.params.id, function(err, flight) {
+        flight.destinations.push(req.body);
+        flight.save(function(err) {
+            res.redirect(`/flights/${flight.id}`);
+        });
+    });
+};
--- routes/destinations.js
+++ routes/destinations.js
@@ -4,4 +4,7 @@ const destinationsCtrl = require('../controllers/destinations');
 
 // The starts with path is '/'
 
+// Post /flights/:id/destinations (create functionality)
+router.post('/flights/:id/destinations', destinationsCtrl.create);
+
 module.exports = router;
